4078688a40552cdb26c53c6018a6c62d4959dc94
flubnugget <32379773+flubnugget@users.noreply.github.com>
2017-12-05 16:58:39 -1000

    Video 6

--- assets/js/mainmenu.js
+++ assets/js/mainmenu.js
@@ -6,12 +6,19 @@ var mainMenuState = {
     
     
     preload: function() { 
+        game.load.spritesheet('startBtn', 'assets/images/start_btn.png', 193, 71);
+        game.stage.backgroundColor= '#8185d5';
+        game.scale.pageAlignHorizontally = true;
+        game.scale.pageAlignVertically = true;
         
     },
     
     
     create: function() {
+        startBtn = game.add.button(game.world.centerX-95, 200, 'startBtn', this.startGame, this, 2, 1, 0);
         
+        var spaceKey = game.input.keyboard.addKey(Phaser.Keyboard.SPACER);
+        spaceKey.onDown.add(this.startGame, this);
     },
     
     
--- assets/js/play.js
+++ assets/js/play.js
@@ -3,11 +3,44 @@
 var playState = {
 
     preload: function() { 
-        
+        // scale the game if it's on a device
+        if (!game.device.desktop) {
+            game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+            game.scale.setMinMax(game.width/2, game.height/2, game.width, game.height);
     },
+        game.scale.pageAlignHorizontally = true;
+        game.scale.pageAlignVertically = true;
+        
+        game.stage.backgroundColor = '#8185d5';
+        
+        //preload pipes and banana
+        game.load.image('bird', 'assets/images/banana.png');
+        game.load.image('pipe', 'assets/images/pipe.png');
+        
+        //load the jump sound
+        game.load.audio('jump', 'assets/audio/jump.m4a');
+    },
+        
 
     create: function() { 
+        // this is the physics function/options
+        game.physics.startSystem(Phaser.Physics.ARDCADE);
         
+        // create our pipes group
+        this.pipes = game.add.group();
+        
+        // our timer
+        this.timer = game.time.events.loop(1500, this.addRowOfPipes, this);
+        
+        //add our bird to the stage
+        this.bird = game.add.sprite(100, 245, 'bird');
+        
+        //enable physics on our burd
+        game.physics.arcade.enable(this.bird);
+        this.bird.body.gravity.y = 1000;
+        
+        //anchor position
+        this.bird.anchor.setTo(-0.2, 0.5);
     },
 
     update: function() {
